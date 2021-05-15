4c91937b87eba6aac40fbdd281b18b934c110e40
Martín Melo <martin.melo.dev.97@gmail.com>
2021-05-15 00:25:29 -0500

    Typo

--- tools/utils.py
+++ tools/utils.py
@@ -8,7 +8,7 @@ def tools_form_validations(form, tool, actual_tool = None):
         tool.mainteance_date = None
 
     # Validate if the forms says that the tool has tracked
-    # If not, we set the tracked if to null (None in Python)
+    # If not, we set the tracked id to null (None in Python)
     if not form.cleaned_data['has_tracker']:
         tool.tracker_id = None
 
@@ -17,7 +17,7 @@ def tools_form_validations(form, tool, actual_tool = None):
     # We need to change the mainteance_date to now
     # Only in the update cases (when actual_tool is not none)
 
-    print('actual_tool', actual_tool)
+    # print('actual_tool', actual_tool)
     if actual_tool is not None:
         if actual_tool \
         and not form.cleaned_data['is_in_maintain']:
